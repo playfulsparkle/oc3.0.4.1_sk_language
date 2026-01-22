97ac3ffed382e48c572b6c32d47801c8127479cd
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-01-22 18:53:23 +0100

    Fix typo for SEO URL query

--- src/upload/admin/language/sk-sk/design/seo_url.php
+++ src/upload/admin/language/sk-sk/design/seo_url.php
@@ -11,21 +11,21 @@ $_['text_filter']             = 'Filter';
 $_['text_default']            = 'Predvolené';
 
 // Column
-$_['column_query']            = 'Dopyt';
+$_['column_query']            = 'Parameter';
 $_['column_keyword']          = 'Kľúčové slová';
 $_['column_store']            = 'Obchod';
 $_['column_language']         = 'Jazyk';
 $_['column_action']           = 'Akcia';
 
 // Entry
-$_['entry_query']             = 'Dopyt';
+$_['entry_query']             = 'Parameter';
 $_['entry_keyword']           = 'Kľúčové slová';
 $_['entry_store']             = 'Obchod';
 $_['entry_language']          = 'Jazyk';
 
 // Error
 $_['error_permission']        = 'Upozornenie: Nemáte oprávnenie upravovať SEO URL!';
-$_['error_query']             = 'Dopyt musí byť dlhší ako 3 znak a kratší než 64 znakov!';
-$_['error_keyword']           = 'Kľúčové slovo musí byť dlhší ako 3 znak a kratší než 64 znakov!';
+$_['error_query']             = 'Parameter musí byť dlhší ako 3 znaky a kratší ako 64 znakov!';
+$_['error_keyword']           = 'Kľúčové slovo musí byť dlhšie ako 3 znaky a kratšie ako 64 znakov!';
 $_['error_exists']            = 'Kľúčové slovo sa už používa!';
-$_['error_query_exists']      = 'Dopyt sa už používa!';
+$_['error_query_exists']      = 'Parameter sa už používa!';
